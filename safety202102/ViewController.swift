3eef88c345edef6ed7c5b6c38773897e6092cb25
sen <ochieeochieeochiee@gmail.com>
2021-02-25 22:58:44 +0900

    Initial Commit

---
+++ safety202102/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  safety202102
+//
+//  Created by Chihiro Nishiwaki on 2021/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
